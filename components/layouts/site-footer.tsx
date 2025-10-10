8e1e508f997f8121522271c9f04c7fe15b31a25f
Tuna Pham <tuangttu90@gmail.com>
2025-10-10 05:46:38 -0400

    Create site-footer.tsx

---
+++ components/layouts/site-footer.tsx
@@ -0,0 +1,7 @@
+export function SiteFooter() {
+  return (
+    <footer className="w-full border-t py-4 text-center text-sm text-muted-foreground">
+      <p>© 2025 My Money. All rights reserved.</p>
+    </footer>
+  )
+}
